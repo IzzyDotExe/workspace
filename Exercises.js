6638454415b31a3c12e60ba2251472cb0535b132
izzydotexe <iaristide@icloud.com>
2023-09-20 18:12:45 +0000

    fix

--- Exercises.js
+++ Exercises.js
@@ -4,10 +4,14 @@ export function Sushi({ name, type }) {
   
   const [count, setCount] = useState(0);
 
+  function HandleClick() {
+    setCount(count+1)
+  }
+
   return (
-    <>
+    <section onClick={HandleClick}>
       <h1>{name}</h1>
       <p>{type}{count}</p>
-    </>
+    </section>
   );
 } 
